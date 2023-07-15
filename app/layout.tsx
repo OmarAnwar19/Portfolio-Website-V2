61044c6e6db75bf7450994a07e081be23260dc20
OmarAnwar19 <omaranwar04@outlook.com>
2023-07-15 16:12:59 -0400

    [feat] made vw 3xl instead of 2xl

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = portfolioMetadata;
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className={clsx("dark", graphik.variable)}>
-      <body className="antialiased max-w-2xl flex flex-col md:flex-row mx-4 mt-8 md:mx-auto">
+      <body className="antialiased max-w-3xl flex flex-col md:flex-row mx-4 mt-8 md:mx-auto">
         <main className="flex-auto min-w-0 mt-6 mb-20 flex flex-col px-2 md:px-0 z-0 overflow-x-hidden">
           <Providers>
             <Navbar />
