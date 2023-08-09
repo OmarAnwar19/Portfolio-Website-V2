aff4463f5ea8d85d9f7d76d1f7e1bc44aa30bfa3
OmarAnwar19 <omaranwar04@outlook.com>
2023-08-09 09:15:21 -0400

    [dev] resume page styles

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -4,7 +4,7 @@ const Resume = () => {
   return (
     <div>
       <iframe
-        className="w-full h-[100vh] mb-6 rounded-lg block border focus:ring-gray-300 focus:border-gray-500 border-gray-300 dark:border-gray-600"
+        className="w-full h-[100vh] mb-6 block border bg-neutral-50/70 dark:bg-neutral-800/70 border-neutral-200 dark:border-neutral-700"
         src="https://docs.google.com/document/d/e/2PACX-1vSKsabRnzPoWngoL73k_Ca01pDCZ7rtM8P9Y-0kh_l_WzD4F8jMnj41sBxmfAx9mq8jZ28UljcgQ17J/pub?embedded=true"
       />
     </div>
