7b32cca84a5bd89ee5ccccc585c508bddbe18bcc
Omar Anwar <omaranwar04@outlook.com>
2025-09-06 13:06:58 -0400

    [fix] moved ico to public folder for cloudflare

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ export const metadata: Metadata = portfolioMetadata;
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className={clsx("dark", graphik.variable)}>
+      <head>
+        <link rel="icon" href="/icon.ico" />
+      </head>
       <body className="antialiased max-w-3xl flex flex-col md:flex-row mx-4 mt-8 md:mx-auto">
         <main className="flex-auto min-w-0 mt-6 mb-20 flex flex-col px-2 z-0 overflow-x-hidden">
           <Providers>
